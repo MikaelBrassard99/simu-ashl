bc4fc5acc0152cee84fdbe2561f76ceb4e8e4598
Mikael <beavers026@gmail.com>
2023-09-25 21:39:43 -0400

    update code from bokeh new version

--- AI_Python/player_data_ashl.py
+++ AI_Python/player_data_ashl.py
@@ -18,16 +18,16 @@ data_frame_forward = pd.read_sql_query('Select * from PlayerInfo where Retire ==
 data_frame_defence = pd.read_sql_query('Select * from PlayerInfo where Retire == "False" and PosD == "True"', con)
 
 # *****for forward******
-df1 = pd.DataFrame(data_frame_forward, columns=["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC",'Name', 'ProTeamName', 'Age', 'SalaryCap', 'PosC', 'PosLW',	'PosRW', 'PosD'])
-df1['St_Mik_off'] = (df1['CK']+df1['ST']+df1['DI']+df1['SK']+df1['DF']+df1['PH']+df1['PA']+df1['SC'])
-df1.sort_values('St_Mik_off', ascending=False)
+df1 = pd.DataFrame(data_frame_forward, columns=["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC",'Name', 'ProTeamName', 'Age', 'SalaryCap', 'PosC', 'PosLW',	'PosRW', 'PosD', 'St_Mik_off','St_Mik_def'])
+# df1['St_Mik_off'] = (df1['CK']+df1['ST']+df1['DI']+df1['SK']+df1['DF']+df1['PH']+df1['PA']+df1['SC'])
+# df1.sort_values('St_Mik_off', ascending=False)
 
 df_ColumnIndexFormat = {}
 index = 0
 for columnName in df1.columns:
     df_ColumnIndexFormat.update({index:columnName})
     index = index+1
-print(df_ColumnIndexFormat)
+# print(df_ColumnIndexFormat)
 
 TOOLTIPS = HoverTool(tooltips=[
     ("Name", "@Name"),
@@ -36,30 +36,30 @@ TOOLTIPS = HoverTool(tooltips=[
     ("Team","@ProTeamName")
 
 ])
+# source.data['St_Mik_off'] = source.data[labels[cb_obj.origin.active[column]]];                               
 
 curdoc().theme = 'dark_minimal'
 source = ColumnDataSource(data=df1)
 LABELS = ["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC"]
-checkbox_button_group = CheckboxButtonGroup(labels=LABELS, active=[0, 1])
-checkbox_button_group.js_on_event("button_click", CustomJS(code="""
+checkbox_button_group = CheckboxButtonGroup(labels=LABELS, active=[])
+checkbox_button_group.js_on_event("button_click", CustomJS(args=dict(source=source), code="""
+    const result = [];
     const labels = ["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC"];
     source.data['St_Mik_off'] = [];
-    for(var column in this.active){
-        console.log(labels[this.active[column]]);
-        console.log(source.data[labels[this.active[column]]]);
-        for(var index in source.data[labels[this.active[column]]]){
-            if(source.data['St_Mik_off'][index] == undefined ){
-                source.data['St_Mik_off'][index] = source.data[labels[this.active[column]]][index];
+    for(var column in cb_obj.origin.active){
+        for(var index in source.data[labels[cb_obj.origin.active[column]]]){
+            if(isNaN(source.data['St_Mik_off'][index])){
+                source.data['St_Mik_off'][index] = source.data[labels[cb_obj.origin.active[column]]][index];
             }else{
-                source.data['St_Mik_off'][index] = source.data['St_Mik_off'][index]+source.data[labels[this.active[column]]][index];
+                source.data['St_Mik_off'][index] = source.data['St_Mik_off'][index]+source.data[labels[cb_obj.origin.active[column]]][index];
             }
         };
     }
     
-    console.log(source.data['St_Mik_off']);
+    console.log(source.data['St_Mik_off'][index]);
     source.change.emit();
 """))
-# checkbox_button_group.js_on_click(CustomJS(args=dict(source=source) , ))
+
 color = linear_cmap(field_name='St_Mik_off', palette=Spectral6 ,low=np.min(source.data['St_Mik_off'][np.nonzero(source.data['St_Mik_off'])]) ,high=np.max(source.data['St_Mik_off']))
 color_bar = ColorBar(color_mapper=color['transform'], width=8)
 plot = figure(width=1500, height=1000, tools=[TOOLTIPS], title="Graphique des Attaquants")
@@ -69,17 +69,19 @@ plot.circle(x='SalaryCap', y='St_Mik_off',line_color=color, size=5, source=sourc
 plot.add_layout(color_bar, 'right')
 
 
-# *****for defence******
-df2 = pd.DataFrame(data_frame_defence, columns=["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC",'Name', 'ProTeamName', 'Age', 'SalaryCap', 'PosC', 'PosLW',	'PosRW', 'PosD'])
-df2['St_Mik_def'] = (df2['CK']+df2['ST']+df2['DI']+df2['SK']+df2['DF']+df2['PH']+df2['PA']+df2['SC'])
-df2.sort_values('St_Mik_def', ascending=False)
+
+
+
+df2 = pd.DataFrame(data_frame_defence, columns=["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC",'Name', 'ProTeamName', 'Age', 'SalaryCap', 'PosC', 'PosLW',	'PosRW', 'PosD', 'St_Mik_off','St_Mik_def'])
+# df2['St_Mik_def'] = (df2['CK']+df2['ST']+df2['DI']+df2['SK']+df2['DF']+df2['PH']+df2['PA']+df2['SC'])
+# df2.sort_values('St_Mik_def', ascending=False)
 
 df_ColumnIndexFormat = {}
 index = 0
 for columnName in df2.columns:
     df_ColumnIndexFormat.update({index:columnName})
     index = index+1
-print(df_ColumnIndexFormat)
+# print(df_ColumnIndexFormat)
 
 TOOLTIPS_def = HoverTool(tooltips=[
     ("index", "$index"),
@@ -94,23 +96,22 @@ TOOLTIPS_def = HoverTool(tooltips=[
 curdoc().theme = 'dark_minimal'
 source_def = ColumnDataSource(data=df2)
 LABELS_def = ["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC"]
-checkbox_button_group_def = CheckboxButtonGroup(labels=LABELS_def, active=[0, 1])
-checkbox_button_group_def.js_on_event("button_click", CustomJS(code="""
+checkbox_button_group_def = CheckboxButtonGroup(labels=LABELS_def, active=[])
+checkbox_button_group_def.js_on_event("button_click", CustomJS(args=dict(source=source_def), code="""
+    const result = [];
     const labels = ["CK", "ST", "DI", "SK", "DF", "PH", "PA", "SC"];
     source.data['St_Mik_def'] = [];
-    for(var column in this.active){
-        console.log(labels[this.active[column]]);
-        console.log(source.data[labels[this.active[column]]]);
-        for(var index in source.data[labels[this.active[column]]]){
-            if(source.data['St_Mik_def'][index] == undefined ){
-                source.data['St_Mik_def'][index] = source.data[labels[this.active[column]]][index];
+    for(var column in cb_obj.origin.active){
+        for(var index in source.data[labels[cb_obj.origin.active[column]]]){
+            if(isNaN(source.data['St_Mik_def'][index])){
+                source.data['St_Mik_def'][index] = source.data[labels[cb_obj.origin.active[column]]][index];
             }else{
-                source.data['St_Mik_def'][index] = source.data['St_Mik_def'][index]+source.data[labels[this.active[column]]][index];
+                source.data['St_Mik_def'][index] = source.data['St_Mik_def'][index]+source.data[labels[cb_obj.origin.active[column]]][index];
             }
         };
     }
     
-    console.log(source.data['St_Mik_def']);
+    console.log(source.data['St_Mik_def'][index]);
     source.change.emit();
 """))
 color_def = linear_cmap(field_name='St_Mik_def', palette=Spectral6 ,low=np.min(source_def.data['St_Mik_def'][np.nonzero(source_def.data['St_Mik_def'])]) ,high=np.max(source_def.data['St_Mik_def']))
