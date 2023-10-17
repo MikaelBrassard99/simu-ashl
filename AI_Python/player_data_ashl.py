101088c818735f359f5ebdf1320ac6cbaa55d18e
Mikael <beavers026@gmail.com>
2023-10-16 23:25:13 -0400

    add scroll to player data view

--- AI_Python/player_data_ashl.py
+++ AI_Python/player_data_ashl.py
@@ -1,13 +1,13 @@
 import sqlite3
 import pandas as pd
 from bokeh.plotting import figure,show
-from  bokeh.models import HoverTool
+from  bokeh.models import HoverTool,BoxZoomTool, ResetTool
 import numpy as np
 from bokeh.io import curdoc
 from bokeh.layouts import column
 from bokeh.models import ColorBar, ColumnDataSource
 from bokeh.palettes import Spectral6
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save
 from bokeh.transform import linear_cmap
 from bokeh.models import CheckboxButtonGroup, CustomJS
 
@@ -29,13 +29,6 @@ for columnName in df1.columns:
     index = index+1
 # print(df_ColumnIndexFormat)
 
-TOOLTIPS = HoverTool(tooltips=[
-    ("Name", "@Name"),
-    ("St_Mik", "@St_Mik_off"),
-    ("SalaryCap","@SalaryCap"),
-    ("Team","@ProTeamName")
-
-])
 # source.data['St_Mik_off'] = source.data[labels[cb_obj.origin.active[column]]];                               
 
 curdoc().theme = 'dark_minimal'
@@ -62,7 +55,7 @@ checkbox_button_group.js_on_event("button_click", CustomJS(args=dict(source=sour
 
 color = linear_cmap(field_name='St_Mik_off', palette=Spectral6 ,low=np.min(source.data['St_Mik_off'][np.nonzero(source.data['St_Mik_off'])]) ,high=np.max(source.data['St_Mik_off']))
 color_bar = ColorBar(color_mapper=color['transform'], width=8)
-plot = figure(width=1500, height=1000, tools=[TOOLTIPS], title="Graphique des Attaquants")
+plot = figure(width=1500, height=1000, tools=[HoverTool(tooltips=[("Name", "@Name"),("Age","@Age"),("St_Mik", "@St_Mik_off"),("SalaryCap","@SalaryCap"),("Team","@ProTeamName")]),BoxZoomTool(),ResetTool()],title="Graphique des Attaquants", toolbar_location="below")
 plot.xaxis.axis_label = 'Salary'
 plot.yaxis.axis_label = 'Somme des statistiques selectionnées'
 plot.circle(x='SalaryCap', y='St_Mik_off',line_color=color, size=5, source=source)
@@ -82,16 +75,6 @@ for columnName in df2.columns:
     df_ColumnIndexFormat.update({index:columnName})
     index = index+1
 # print(df_ColumnIndexFormat)
-
-TOOLTIPS_def = HoverTool(tooltips=[
-    ("index", "$index"),
-    ("Name", "@Name"),
-    ("St_Mik", "@St_Mik_def"),
-    ("SalaryCap","@SalaryCap"),
-    ("Team","@ProTeamName")
-
-])
-
 
 curdoc().theme = 'dark_minimal'
 source_def = ColumnDataSource(data=df2)
@@ -116,9 +99,11 @@ checkbox_button_group_def.js_on_event("button_click", CustomJS(args=dict(source=
 """))
 color_def = linear_cmap(field_name='St_Mik_def', palette=Spectral6 ,low=np.min(source_def.data['St_Mik_def'][np.nonzero(source_def.data['St_Mik_def'])]) ,high=np.max(source_def.data['St_Mik_def']))
 color_bar_def = ColorBar(color_mapper=color_def['transform'], width=8)
-plot_def = figure(width=1500, height=1000, tools=[TOOLTIPS_def], title="Graphique des défenseurs")
+plot_def = figure(width=1500, height=1000, tools=[HoverTool(tooltips=[("Name", "@Name"),("Age","@Age"),("St_Mik", "@St_Mik_def"),("SalaryCap","@SalaryCap"),("Team","@ProTeamName")]),BoxZoomTool(),ResetTool()], title="Graphique des défenseurs", toolbar_location="below")
 plot_def.xaxis.axis_label = 'Salary'
 plot_def.yaxis.axis_label = 'Somme des statistiques selectionnées'
+
 plot_def.circle(x='SalaryCap', y='St_Mik_def',line_color=color_def, size=5, source=source_def)
 plot_def.add_layout(color_bar_def, 'right')
+output_file(filename="billy_beane.html", title="ASHL DATA FILE")
 show(column(checkbox_button_group,plot,checkbox_button_group_def,plot_def))
