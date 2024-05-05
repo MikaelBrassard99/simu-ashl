279f275c237544f9bb6208877c59f95b3074bff1
MikaelBrassard99 <beavers026@gmail.com>
2024-05-04 23:59:01 -0400

    working on line management

--- static/script.js
+++ static/script.js
@@ -245,3 +245,60 @@ function updateRadarChart(avg_league_stat_def, avg_league_stat_fwd) {
     playerChart.update();
   });
 }
+
+//dragable functions
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+  
+    dragSrcEl = this;
+  
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+  
+  function handleDrop(e) {
+    e.stopPropagation();
+  
+    if (dragSrcEl !== this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+  
+    return false;
+  }
+    
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragover', handleDragOver);
+    item.addEventListener('dragenter', handleDragEnter);
+    item.addEventListener('dragleave', handleDragLeave);
+    item.addEventListener('dragend', handleDragEnd);
+    item.addEventListener('drop', handleDrop);
+  });
+});
